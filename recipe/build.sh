3c0de963aa22e757ce2cc51365b44b26bc224814
Pariterre <pariterre@hotmail.com>
2020-04-12 13:06:30 -0400

    Put back eigen3

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,13 +7,12 @@ cmake .. \
       -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_PREFIX_PATH=$PREFIX \
-      -DMATH_LIBRARY_BACKEND="Casadi" \
+      -DMATH_LIBRARY_BACKEND="Eigen3" \
       -DBUILD_EXAMPLE=OFF \
       -DMODULE_ACTUATORS=ON \
       -DMODULE_MUSCLES=ON \
       -DBINDER_PYTHON3=ON \
         -DNUMPY_INCLUDES=$PREFIX/numpy/core/include \
-        -DPython_EXECUTABLE=$PREFIX/bin/python \
         -DPython3_EXECUTABLE=$PREFIX/bin/python \
       -DBINDER_MATLAB=OFF \
         -DMatlab_ezc3d_INSTALL_DIR=$PREFIX/MATLAB
