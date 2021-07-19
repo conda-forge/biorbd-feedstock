fde7d48ea770812bfabeae87b706ae14365a5d9a
Pariterre <pariterre@hotmail.com>
2021-07-19 15:08:49 +0200

    Merged eigen and casadi into one build

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,13 +1,32 @@
 #!/bin/bash
-mkdir build
-cd build
+mkdir build_casadi
+cd build_casadi
 
 cmake .. \
       -GNinja \
       -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_PREFIX_PATH=$PREFIX \
-      -DMATH_LIBRARY_BACKEND=$backend_name_in_build \
+      -DMATH_LIBRARY_BACKEND="Casadi" \
+      -DBUILD_EXAMPLE=OFF \
+      -DMODULE_ACTUATORS=ON \
+      -DMODULE_MUSCLES=ON \
+      -DBINDER_PYTHON3=ON \
+        -DPython3_EXECUTABLE=$PREFIX/bin/python \
+      -DBINDER_MATLAB=OFF
+
+ninja install
+
+cd ..
+mkdir build_eigen
+cd build_eigen
+
+cmake .. \
+      -GNinja \
+      -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_PREFIX_PATH=$PREFIX \
+      -DMATH_LIBRARY_BACKEND=$"Eigen3" \
       -DBUILD_EXAMPLE=OFF \
       -DMODULE_ACTUATORS=ON \
       -DMODULE_MUSCLES=ON \
