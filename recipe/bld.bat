0e5d7586a3123d6920576bae95372263e93c7039
Pariterre <benjamin.michaud.phq@gmail.com>
2025-09-25 13:33:02 -0400

    Refactor bld.bat for improved CMake configuration

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,27 +1,43 @@
+@echo off
+echo Compiling biorbd...
+echo %PREFIX%
+
+rem ================================
+rem Build CasADi backend
+rem ================================
+echo ... with CasADi backend
 mkdir build_casadi
 cd build_casadi
 
-cmake ../^
-    -GNinja ^
-    -DCMAKE_INSTALL_PREFIX="%PREFIX%"^
-    -DCMAKE_BUILD_TYPE=Release^
-    -DMATH_LIBRARY_BACKEND="Casadi"^
-  	-DBUILD_EXAMPLE=OFF^
-    -DBINDER_PYTHON3=ON^
-      -DPYTHON_EXECUTABLE="%PREFIX%/python.exe"
+cmake .. ^
+  -GNinja ^
+  -DCMAKE_INSTALL_PREFIX=%PREFIX% ^
+  -DCMAKE_PREFIX_PATH=%PREFIX% ^
+  -DCMAKE_BUILD_TYPE=Release ^
+  -DMATH_LIBRARY_BACKEND=Casadi ^
+  -DBUILD_EXAMPLE=OFF ^
+  -DBINDER_PYTHON3=ON ^
+    -DPython3_EXECUTABLE=%PREFIX%\python.exe
 ninja install
 
-
 cd ..
+
+rem ================================
+rem Build Eigen3 backend
+rem ================================
+echo ... with Eigen3 backend
 mkdir build_eigen
 cd build_eigen
 
-cmake ../^
-    -GNinja ^
-    -DCMAKE_INSTALL_PREFIX="%PREFIX%"^
-    -DCMAKE_BUILD_TYPE=Release^
-    -DMATH_LIBRARY_BACKEND="Eigen3"^
-  	-DBUILD_EXAMPLE=OFF^
-    -DBINDER_PYTHON3=ON^
-      -DPYTHON_EXECUTABLE="%PREFIX%/python.exe"
+cmake .. ^
+  -GNinja ^
+  -DCMAKE_INSTALL_PREFIX=%PREFIX% ^
+  -DCMAKE_PREFIX_PATH=%PREFIX% ^
+  -DCMAKE_BUILD_TYPE=Release ^
+  -DMATH_LIBRARY_BACKEND=Eigen3 ^
+  -DBUILD_EXAMPLE=OFF ^
+  -DBINDER_PYTHON3=ON ^
+    -DPython3_EXECUTABLE=%PREFIX%\python.exe
 ninja install
+
+cd ..
