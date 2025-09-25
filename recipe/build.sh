e93d8ed3a53f8c7eb63e6082653e7dfd64c1a314
Pariterre <benjamin.michaud.phq@gmail.com>
2025-09-25 13:05:23 -0400

    Fix formatting in build.sh for cmake commands

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,14 +8,14 @@ mkdir build_casadi
 cd build_casadi
 
 cmake .. \
-  -GNinja \
+  -GNinja
   -DCMAKE_INSTALL_PREFIX=$PREFIX \
+  -DCMAKE_PREFIX_PATH=$PREFIX \
   -DCMAKE_BUILD_TYPE=Release \
   -DMATH_LIBRARY_BACKEND="Casadi" \
   -DBUILD_EXAMPLE=OFF \
   -DBINDER_PYTHON3=ON \
     -DPython3_EXECUTABLE=$PREFIX/bin/python
-
 ninja install
 
 cd ..
@@ -25,9 +25,10 @@ echo "... with Eigen3 backend"
 mkdir build_eigen
 cd build_eigen
 
-cmake .. \ 
-  -GNinja \
+cmake .. \
+  -GNinja
   -DCMAKE_INSTALL_PREFIX=$PREFIX \
+  -DCMAKE_PREFIX_PATH=$PREFIX \
   -DCMAKE_BUILD_TYPE=Release \
   -DMATH_LIBRARY_BACKEND="Eigen3" \
   -DBUILD_EXAMPLE=OFF \
