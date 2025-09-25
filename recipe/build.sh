639b849ff906dfea635cb22a75214c4984d3d658
Pariterre <benjamin.michaud.phq@gmail.com>
2025-09-25 10:21:32 -0400

    Refactor build script by simplifying cmake options
    Removed unused options from cmake commands for casadi and eigen builds.

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,17 +4,13 @@
 mkdir build_casadi
 cd build_casadi
 
-cmake . .. \
+cmake .. \
       -GNinja \
       -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_BUILD_TYPE=Release \
-      -DCMAKE_PREFIX_PATH=$PREFIX \
       -DMATH_LIBRARY_BACKEND="Casadi" \
       -DBUILD_EXAMPLE=OFF \
-      -DMODULE_ACTUATORS=ON \
-      -DMODULE_MUSCLES=ON \
       -DBINDER_PYTHON3=ON \
-      -DBINDER_MATLAB=OFF \
       -DPython3_EXECUTABLE=$PREFIX/bin/python
 
 ninja install
@@ -25,17 +21,13 @@ cd ..
 mkdir build_eigen
 cd build_eigen
 
-cmake . .. \
+cmake .. \
       -GNinja \
       -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_BUILD_TYPE=Release \
-      -DCMAKE_PREFIX_PATH=$PREFIX \
-      -DMATH_LIBRARY_BACKEND=$"Eigen3" \
+      -DMATH_LIBRARY_BACKEND="Eigen3" \
       -DBUILD_EXAMPLE=OFF \
-      -DMODULE_ACTUATORS=ON \
-      -DMODULE_MUSCLES=ON \
       -DBINDER_PYTHON3=ON \
-      -DBINDER_MATLAB=OFF \
       -DPython3_EXECUTABLE=$PREFIX/bin/python
-
+      
 ninja install
