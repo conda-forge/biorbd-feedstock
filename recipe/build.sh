f6b5d7f01501ecccf8c5a26bb81449d6ad94d453
Pariterre <benjamin.michaud.phq@gmail.com>
2025-09-25 12:30:19 -0400

    Refactor build script for improved readability

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,33 +1,35 @@
 #!/bin/bash
+echo "Compiling biorbd..."
+echo "PREFIX=$PREFIX"
 
 # Build casadi
+echo "... with CasADi backend"
 mkdir build_casadi
 cd build_casadi
 
-cmake .. \
-      -GNinja \
-      -DCMAKE_INSTALL_PREFIX=$PREFIX \
-      -DCMAKE_BUILD_TYPE=Release \
-      -DMATH_LIBRARY_BACKEND="Casadi" \
-      -DBUILD_EXAMPLE=OFF \
-      -DBINDER_PYTHON3=ON \
-      -DPython3_EXECUTABLE=$PREFIX/bin/python
-
+cmake .. \ 
+  -GNinja \
+  -DCMAKE_INSTALL_PREFIX=$PREFIX \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DMATH_LIBRARY_BACKEND="Casadi" \
+  -DBUILD_EXAMPLE=OFF \
+  -DBINDER_PYTHON3=ON \
+    -DPython3_EXECUTABLE=$PREFIX/bin/python
 ninja install
 
 cd ..
 
 # Build Eigen
+echo "... with Eigen3 backend"
 mkdir build_eigen
 cd build_eigen
 
-cmake .. \
-      -GNinja \
-      -DCMAKE_INSTALL_PREFIX=$PREFIX \
-      -DCMAKE_BUILD_TYPE=Release \
-      -DMATH_LIBRARY_BACKEND="Eigen3" \
-      -DBUILD_EXAMPLE=OFF \
-      -DBINDER_PYTHON3=ON \
-      -DPython3_EXECUTABLE=$PREFIX/bin/python
-      
+cmake .. \ 
+  -GNinja \
+  -DCMAKE_INSTALL_PREFIX=$PREFIX \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DMATH_LIBRARY_BACKEND="Eigen3" \
+  -DBUILD_EXAMPLE=OFF \
+  -DBINDER_PYTHON3=ON \
+    -DPython3_EXECUTABLE=$PREFIX/bin/python
 ninja install
