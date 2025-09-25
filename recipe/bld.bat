5c72f06e106378b46e643cead4757b9467165b4b
Pariterre <benjamin.michaud.phq@gmail.com>
2025-09-25 10:23:43 -0400

    Updated bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,29 +3,25 @@ cd build_casadi
 
 cmake ../^
     -GNinja ^
-    -DCMAKE_BUILD_TYPE=Release^
     -DCMAKE_INSTALL_PREFIX="%PREFIX%"^
+    -DCMAKE_BUILD_TYPE=Release^
     -DMATH_LIBRARY_BACKEND="Casadi"^
   	-DBUILD_EXAMPLE=OFF^
-    -DMODULE_ACTUATORS=ON^
-    -DMODULE_MUSCLES=ON^
     -DBINDER_PYTHON3=ON^
-      -DPYTHON_EXECUTABLE="%PREFIX%/python.exe"^
-   	-DBINDER_MATLAB=OFF
+      -DPYTHON_EXECUTABLE="%PREFIX%/python.exe"
 ninja install
+
 
 cd ..
 mkdir build_eigen
 cd build_eigen
+
 cmake ../^
     -GNinja ^
-    -DCMAKE_BUILD_TYPE=Release^
     -DCMAKE_INSTALL_PREFIX="%PREFIX%"^
+    -DCMAKE_BUILD_TYPE=Release^
     -DMATH_LIBRARY_BACKEND="Eigen3"^
   	-DBUILD_EXAMPLE=OFF^
-    -DMODULE_ACTUATORS=ON^
-    -DMODULE_MUSCLES=ON^
     -DBINDER_PYTHON3=ON^
-      -DPYTHON_EXECUTABLE="%PREFIX%/python.exe"^
-   	-DBINDER_MATLAB=OFF
+      -DPYTHON_EXECUTABLE="%PREFIX%/python.exe"
 ninja install
